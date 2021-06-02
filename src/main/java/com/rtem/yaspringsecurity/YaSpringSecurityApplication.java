28a02b70b0bc1c792578ee6d5afd7fa767ce9f24
artem.zatula <artem.zatula@x5.ru>
2021-06-02 15:38:13 +0300

    Initial commit

---
+++ src/main/java/com/rtem/yaspringsecurity/YaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rtem.yaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtem/yaspringsecurity/YaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtem.yaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
