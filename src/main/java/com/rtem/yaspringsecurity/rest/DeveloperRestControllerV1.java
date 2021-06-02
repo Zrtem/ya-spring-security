58cd42a7eb596a78f19cf003fc2373efbafcba81
artem.zatula <artem.zatula@x5.ru>
2021-06-02 16:38:12 +0300

    default SpringSecurityConfig

---
+++ src/main/java/com/rtem/yaspringsecurity/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,34 @@
+package com.rtem.yaspringsecurity.rest;
+
+import com.rtem.yaspringsecurity.model.Developer;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+    private List<Developer> developers = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Petr", "Petrov"),
+            new Developer(3L, "Sergey", "Sergeev")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return developers.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
